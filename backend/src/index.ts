fa4ba9465e4295df4bedc10c6ac7e067b0f2560f
bitbard3 <ansharora3839@gmail.com>
2024-11-11 04:40:49 +0530

    Create signin route

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,9 +1,22 @@
 import { Hono } from "hono";
 import userRouter from "./routes/user.routes";
-import workerRouter from './routes/worker.routes'
+import workerRouter from "./routes/worker.routes";
 
 const app = new Hono().basePath("/api");
-app.route("user", userRouter);
-app.route("worker",workerRouter)
+
+app.route("/user", userRouter);
+app.route("/worker", workerRouter);
+
+app.all("*", (c) => {
+  console.log("Received request for:", c.req.url);
+  return c.json(
+    {
+      error: "Route not found",
+      requestedPath: c.req.path,
+      method: c.req.method,
+    },
+    404
+  );
+});
 
 export default app;
--- backend/src/routes/user.routes.ts
+++ backend/src/routes/user.routes.ts
@@ -1,18 +1,61 @@
 import { Hono } from "hono";
 import { getSignedDFileUrl } from "../utils/s3/getSignedFileUrl";
+import { sign } from "hono/jwt";
+import prisma from "../../prisma/db";
+
 const user = new Hono();
 
+user.post("/signin", async (c) => {
+ 
+  // TODO: add sign verification logic here
+  if (!process.env.JWT_SECRET) {
+    throw new Error("JWT secret doesnt exist");
+  }
+  const hardCodedAddress = "CcrCWF9bh4D4NrdSyEtCsWfoc5oMgHvCAjYMAUAkmHmt";
+  let existingUser;
+  try {
+    existingUser = await prisma.user.findUnique({
+      where: {
+        address: hardCodedAddress,
+      },
+    });
+  } catch (error) {
+   return c.json({msg:"Something went wrong"},500)
+  }
+  if(existingUser){
+    const token = await sign({userId:existingUser.id},process.env.JWT_SECRET)
+    return c.json({token})
+  }
+  else{
+    try {
+      const user = await prisma.user.create({
+        data:{
+          address:hardCodedAddress
+        }
+      })
+      const token = await sign({userId:user.id},process.env.JWT_SECRET)
+      return c.json({token})
+
+    } catch (error) {
+      return c.json({msg:"Something went wrong"},500) 
+    }
+  }
+
+});
+
 user.get("/signedurl", async (c) => {
+  //TODO:get user id from db(setup auth)
+
   const userId = 1;
   try {
-    const fileType = c.req.query('filetype') || 'jpg';
+    const fileType = c.req.query("filetype") || "jpg";
     const url = await getSignedDFileUrl({
       fileName: `${userId}/${Math.random()}/${Date.now()}.${fileType}`,
       expiresIn: 60 * 10,
     });
-    return c.json({url}, 200);
+    return c.json({ url }, 200);
   } catch (error) {
-    return c.json({ error: 'Internal server error' }, 500);
+    return c.json({ error: "Internal server error" }, 500);
   }
 });
 
