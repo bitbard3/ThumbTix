645e5ce2dd2479b1c031b22f7e285f2dbde3de78
bitbard3 <ansharora3839@gmail.com>
2024-11-11 16:26:06 +0530

    Add route to fetch worker balance

--- backend/src/routes/worker.routes.ts
+++ backend/src/routes/worker.routes.ts
@@ -1,10 +1,11 @@
 import { Hono } from "hono";
 import { sign } from "hono/jwt";
 import prisma from "../../prisma/db";
+import { authMiddleware } from "../middlewares/auth.middleware";
 
 const worker = new Hono();
 
-worker.post("/signin",async(c)=>{
+worker.post("/signin", async (c) => {
   // TODO: add sign verification logic here
   if (!process.env.JWT_SECRET) {
     throw new Error("JWT secret doesnt exist");
@@ -13,9 +14,9 @@ worker.post("/signin",async(c)=>{
   let existingUser;
   try {
     existingUser = await prisma.worker.findUnique({
-        where:{
-            address:hardCodedAddress
-        }
+      where: {
+        address: hardCodedAddress,
+      },
     });
   } catch (error) {
     return c.json({ msg: "Something went wrong" }, 500);
@@ -28,30 +29,48 @@ worker.post("/signin",async(c)=>{
     return c.json({ token });
   } else {
     try {
-      const user = await prisma.$transaction (async tx=>{
+      const user = await prisma.$transaction(async (tx) => {
         const balance = await tx.balance.create({
-            data:{
-                lockedAmount:"0",
-                pendingAmount:"0"
-            }
-        })
+          data: {
+            lockedAmount: "0",
+            pendingAmount: "0",
+          },
+        });
         const user = await tx.worker.create({
-            data: {
-              address: hardCodedAddress,
-              balanceId:balance.id
-            },
-          });
-          return user
-      })
-     
+          data: {
+            address: hardCodedAddress,
+            balanceId: balance.id,
+          },
+        });
+        return user;
+      });
+
       const token = await sign({ userId: user.id }, process.env.JWT_SECRET);
       return c.json({ token });
     } catch (error) {
       return c.json({ msg: "Something went wrong" }, 500);
-      console.log(error)
     }
   }
 });
 
+worker.get("/balance", authMiddleware, async (c) => {
+  const userId = c.user?.userId;
+  if (!userId) {
+    return c.json({ error: "User not found in context" }, 403);
+  }
+  try {
+    const balance = await prisma.worker.findUnique({
+      where: {
+        id: Number(userId),
+      },
+      include: {
+        balance: true,
+      },
+    });
+    return c.json({ balance: balance?.balance }, 200);
+  } catch (error) {
+    return c.json({ msg: "Something went wrong" }, 500);
+  }
+});
 
-export default worker
+export default worker;
