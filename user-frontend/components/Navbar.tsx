46aebb774b4d1cca391029fa200639dc38434d87
bitbard3 <ansharora3839@gmail.com>
2024-11-14 08:21:28 +0530

    Improve UI of navbar and remove logo

--- user-frontend/components/Navbar.tsx
+++ user-frontend/components/Navbar.tsx
@@ -1,19 +1,12 @@
 import React from "react";
-import Image from "next/image";
-import icon from "../public/icon.svg";
 
 export default function Navbar() {
   return (
-    <div className=" absolute top-0 w-full">
+    <div className=" absolute top-0 w-full border-b border-neutral-700 border-opacity-70">
       <div className="w-full  main__container">
-        <div className="inner__container rounded-md py-2 flex items-center justify-between">
+        <div className="inner__container rounded-md py-4 flex items-center justify-between">
           <div className="flex items-center">
             <h3 className="text-white font-medium text-lg">ThumbTix</h3>
-            <Image
-              src={icon}
-              alt="Icon image"
-              className="size-10 font-bold mt-2"
-            />
           </div>
         </div>
       </div>
