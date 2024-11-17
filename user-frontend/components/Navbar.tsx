094cf161a9a8ef0ea7fe330d3905a980503e834a
bitbard3 <ansharora3839@gmail.com>
2024-11-17 09:26:07 +0530

    Minor layout changes

--- user-frontend/components/Navbar.tsx
+++ user-frontend/components/Navbar.tsx
@@ -2,9 +2,9 @@ import React from "react";
 
 export default function Navbar() {
   return (
-    <div className=" absolute top-0 w-full border-b border-neutral-700 border-opacity-70">
-      <div className="w-full  main__container">
-        <div className="inner__container rounded-md py-4 flex items-center justify-between">
+    <div className=" absolute  top-0 w-full border-b border-neutral-700 border-opacity-70">
+      <div className="w-full h-full  main__container">
+        <div className="inner__container max-h-[150px] rounded-md py-4 flex items-center justify-between">
           <div className="flex items-center">
             <h3 className="text-white font-medium text-lg">ThumbTix</h3>
           </div>
