0b134277256cb7a20068363b6a044cce2b18d59e
bitbard3 <ansharora3839@gmail.com>
2024-11-18 14:28:58 +0530

    Add task endpoint to create task

---
+++ user-frontend/app/(root)/task/page.tsx
@@ -0,0 +1,22 @@
+import { TaskForm } from "@/components/TaskForm";
+import { StaggeredBlurIn } from "@/components/ui/StaggeredBlurIn";
+import React from "react";
+
+export default function Task() {
+  return (
+    <div className="bg-black min-h-screen w-screen overflow-hidden">
+      <StaggeredBlurIn staggerDelay={0.3} duration={0.8}>
+        <div className="main__container w-full">
+          <div className="flex flex-col w-full min-h-screen pt-[120px] inner__container">
+            <p className="text-white text-3xl font-medium pb-10 text-center">
+              Create a task
+            </p>
+            <div className="mx-auto">
+              <TaskForm />
+            </div>
+          </div>
+        </div>
+      </StaggeredBlurIn>
+    </div>
+  );
+}
