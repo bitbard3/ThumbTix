f7eba6b590717122dc97a5118b5244e2f8950ae4
bitbard3 <ansharora3839@gmail.com>
2024-11-19 18:00:43 +0530

    Display all the user task and its status

---
+++ user-frontend/app/(root)/user/[userId]/page.tsx
@@ -0,0 +1,58 @@
+import { Badge } from "@/components/ui/badge";
+import { StaggeredBlurIn } from "@/components/ui/StaggeredBlurIn";
+import { getTask } from "@/lib/api";
+import Image from "next/image";
+import React from "react";
+
+export default async function UserTask() {
+  const tasks = await getTask();
+  console.log(tasks);
+  return (
+    <div className="bg-black min-h-screen w-screen overflow-hidden">
+      <StaggeredBlurIn staggerDelay={0.3} duration={0.8}>
+        <div className="main__container w-full">
+          <div className="flex flex-col w-full min-h-screen pt-[120px] inner__container">
+            <p className="text-white text-3xl font-medium pb-10">Your tasks</p>
+            {tasks.map((task) => (
+              <div
+                key={task.id}
+                className="w-full rounded-lg mb-8 p-4 bg-gray-800"
+              >
+                <p className="text-white text-xl">{task.title}</p>
+                <div className="flex mt-3 space-x-2">
+                  <Badge variant={`${task.done ? `success` : `warning`}`}>
+                    {task.done ? `Done` : `Work in progress`}
+                  </Badge>
+                  <Badge className="bg-neutral-900" variant={"secondary"}>
+                    {Number(task.amount) / 1000000000} SOL
+                  </Badge>
+                </div>
+                <div className="flex w-full justify-between mt-5 flex-wrap  space-x-[2%]">
+                  {task.options.map((option) => (
+                    <div
+                      key={option.imageUrl}
+                      className="w-[28%] rounded-lg bg-slate-600 justify-between flex flex-col items-center space-y-2 p-5"
+                    >
+                      <Image
+                        className="w-full h-auto"
+                        src={option.imageUrl}
+                        alt="Image option"
+                        layout="responsive"
+                        width={16}
+                        height={9}
+                      />
+
+                      <p className="text-white font-medium pt-4">
+                        {option._count.submissions} Votes
+                      </p>
+                    </div>
+                  ))}
+                </div>
+              </div>
+            ))}
+          </div>
+        </div>
+      </StaggeredBlurIn>
+    </div>
+  );
+}
