db99da16680871af80e8a24da02b992917d4dd8e
bitbard3 <ansharora3839@gmail.com>
2024-12-07 02:03:48 +0530

    Add cdn link to next config

--- worker-frontend/next.config.ts
+++ worker-frontend/next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "d2aebbcnff6b6k.cloudfront.net",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
