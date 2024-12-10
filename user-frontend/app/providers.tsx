f7fc099d1a2d3ccca4444960c6e9b7aa16179815
bitbard3 <ansharora3839@gmail.com>
2024-12-10 19:31:57 +0530

    Create providers for wallet adapters context

---
+++ user-frontend/app/providers.tsx
@@ -0,0 +1,25 @@
+"use client";
+import React, { useMemo } from "react";
+import {
+  ConnectionProvider,
+  WalletProvider,
+} from "@solana/wallet-adapter-react";
+import { WalletAdapterNetwork } from "@solana/wallet-adapter-base";
+import { WalletModalProvider } from "@solana/wallet-adapter-react-ui";
+
+// Default styles that can be overridden by your app
+import "@solana/wallet-adapter-react-ui/styles.css";
+export default function Providers({ children }: { children: React.ReactNode }) {
+  const network = WalletAdapterNetwork.Testnet;
+  const endpoint = process.env.NEXT_PUBLIC_RPC_URL;
+  const wallets = useMemo(() => [], [network]);
+  return (
+    <>
+      <ConnectionProvider endpoint={endpoint}>
+        <WalletProvider wallets={wallets} autoConnect>
+          <WalletModalProvider>{children}</WalletModalProvider>
+        </WalletProvider>
+      </ConnectionProvider>
+    </>
+  );
+}
