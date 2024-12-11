da221e595f952a3720f2a62f1a81a1ebb89c61e7
bitbard3 <ansharora3839@gmail.com>
2024-12-12 01:02:04 +0530

    Change network config

--- user-frontend/app/providers.tsx
+++ user-frontend/app/providers.tsx
@@ -9,7 +9,7 @@ import { WalletModalProvider } from "@solana/wallet-adapter-react-ui";
 import "@solana/wallet-adapter-react-ui/styles.css";
 
 export default function Providers({ children }: { children: React.ReactNode }) {
-  const network = WalletAdapterNetwork.Testnet;
+  const network = WalletAdapterNetwork.Devnet;
   const endpoint = process.env.NEXT_PUBLIC_RPC_URL;
   const wallets = useMemo(() => [], [network]);
   return (
