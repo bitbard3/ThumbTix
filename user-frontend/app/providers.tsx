a49be0a82515ec22a26c4a96f12e784542d7ff33
bitbard3 <ansharora3839@gmail.com>
2024-12-11 18:31:59 +0530

    Feature: Add connecting wallet and verifying the message with backend

--- user-frontend/app/providers.tsx
+++ user-frontend/app/providers.tsx
@@ -6,9 +6,8 @@ import {
 } from "@solana/wallet-adapter-react";
 import { WalletAdapterNetwork } from "@solana/wallet-adapter-base";
 import { WalletModalProvider } from "@solana/wallet-adapter-react-ui";
-
-// Default styles that can be overridden by your app
 import "@solana/wallet-adapter-react-ui/styles.css";
+
 export default function Providers({ children }: { children: React.ReactNode }) {
   const network = WalletAdapterNetwork.Testnet;
   const endpoint = process.env.NEXT_PUBLIC_RPC_URL;
