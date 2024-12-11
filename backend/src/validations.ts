d740bd9fa30d49047fc1ae1db02c2202234d9f9e
bitbard3 <ansharora3839@gmail.com>
2024-12-11 18:29:51 +0530

    Create signin input schema for signin route

--- backend/src/validations.ts
+++ backend/src/validations.ts
@@ -1,13 +1,45 @@
-import {z} from 'zod'
+import { z } from "zod";
+import bs58 from "bs58";
 
 export const createTaskSchema = z.object({
-    title:z.string().optional(),
-    transactionSignature:z.string(),
-    options:z.array(z.object({
-        imageUrl:z.string()
-    }))
-})
+  title: z.string().optional(),
+  transactionSignature: z.string(),
+  options: z.array(
+    z.object({
+      imageUrl: z.string(),
+    })
+  ),
+});
 export const createSubmissionSchema = z.object({
-    taskId:z.number(),
-    optionId:z.number()
-})
+  taskId: z.number(),
+  optionId: z.number(),
+});
+
+export const verifySiginSchema = z.object({
+  publicKey: z.string().refine(
+    (address) => {
+      try {
+        bs58.decode(address);
+        return true;
+      } catch {
+        return false;
+      }
+    },
+    {
+      message: "Invalid base58 encoded string",
+    }
+  ),
+  signature: z.string().refine(
+    (signature) => {
+      try {
+        return (
+          Buffer.from(signature, "base64").toString("base64") === signature
+        );
+      } catch {
+        return false;
+      }
+    },
+    { message: "Signature must be a valid Base64 encoded string" }
+  ),
+  message: z.string(),
+});
