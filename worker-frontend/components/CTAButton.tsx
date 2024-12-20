478b7a998f1c06e397fe1bcddecbac72d258083d
bitbard3 <ansharora3839@gmail.com>
2024-12-20 06:55:44 +0530

    Create CTA Button components to wrap shiny button for client side operations

---
+++ worker-frontend/components/CTAButton.tsx
@@ -0,0 +1,30 @@
+"use client";
+import React from "react";
+import ShinyButton from "@/components/ui/ShinyButton";
+import { MoveRight } from "lucide-react";
+import { useRouter } from "next/navigation";
+import { useWalletModal } from "@solana/wallet-adapter-react-ui";
+export default function CTAButton() {
+  const { setVisible } = useWalletModal();
+  const router = useRouter();
+  const handleClick = async () => {
+    if (localStorage.getItem("token")) {
+      router.push("/user/task");
+    } else {
+      if (localStorage.getItem("walletName")) {
+        return;
+      }
+      setVisible(true);
+    }
+  };
+  return (
+    <ShinyButton onClick={handleClick} className="mt-12">
+      Get Started{" "}
+      <MoveRight
+        className="ml-1.5"
+        strokeWidth={1}
+        color="rgb(255,255,255,90%)"
+      />{" "}
+    </ShinyButton>
+  );
+}
