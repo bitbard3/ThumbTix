ca335af1677e28a1fe9c691980c2394caee779ba
bitbard3 <ansharora3839@gmail.com>
2024-12-22 18:06:52 +0530

    Add footer to worker frontend

--- worker-frontend/app/page.tsx
+++ worker-frontend/app/page.tsx
@@ -1,13 +1,14 @@
 import CTAButton from "@/components/CTAButton";
 import { StaggeredBlurIn } from "@/components/ui/StaggeredBlurIn";
+import Link from "next/link";
 
 export default function Home() {
   return (
     <>
       <div className="bg-black min-h-screen w-screen overflow-hidden">
         <StaggeredBlurIn staggerDelay={0.3} duration={0.8}>
-          <div className="main__container w-full ">
-            <div className="flex flex-col w-full min-h-screen items-center justify-center inner__container">
+          <div className="main__container w-full min-h-screen flex flex-col">
+            <div className="flex flex-col w-full flex-grow items-center justify-center inner__container">
               <p className="text-white text-5xl lg:text-7xl font-[600] text-center">
                 Earn by analysing images
               </p>
@@ -15,6 +16,19 @@ export default function Home() {
                 Earn Solana by choosing the best images, simple, fun, rewarding!
               </p>
               <CTAButton />
+            </div>
+            <div className="mt-auto py-8 flex border-t w-full items-center">
+              <p className="text-white">
+                Create your own{" "}
+                <Link
+                  target="_blank"
+                  href={"https://thumb-tix-user.vercel.app/"}
+                >
+                  <span className="font-medium border-b border-neutral-200">
+                    Task
+                  </span>{" "}
+                </Link>
+              </p>
             </div>
           </div>
         </StaggeredBlurIn>
